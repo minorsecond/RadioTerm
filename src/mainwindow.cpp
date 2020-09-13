7a948d3026054d4177415bad721744e2e318fcca
Ross Wardrup <minorsecond@gmail.com>
2020-09-13 12:17:26 -0500

    Add user settings interface

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,4 +10,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
      * Set up the main window.
      */
     ui.setupUi(this);
+
+    // Set up menubar items
+    QMenu *edit_menu = menuBar()->addMenu("Edit");
+    auto *user_settings = new QAction("User Settings");
+    edit_menu->addAction(user_settings);
 }
